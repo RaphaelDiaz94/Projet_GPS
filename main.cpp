b5e3f5d4266d0b4f26b318d9590c7bea0414a4d5
Raphael <raphael.diaz@ynov.com>
2021-02-10 12:01:19 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Temporaire1.h"
+#include "Connection.h"
+
+
+
+
+using namespace std;
+
+int main()
+{
+    Temporaire1 T1;
+    T1.connection();
+    T1.liste1();
+    T1.liste2();
+    T1.liste3();
+    T1.liste4();
+    T1.liste5();
+    T1.affichage();
+
+};

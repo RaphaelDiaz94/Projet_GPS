b6f5b85546ada7ef200206a1dedb02474e206b1e
Raphael <raphael.diaz@ynov.com>
2021-02-10 12:01:23 +0100

    Create Connection.h

---
+++ include/Connection.h
@@ -0,0 +1,17 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+#include <mysql.h>
+using namespace std;
+
+
+
+
+class Connection
+{
+    public:
+
+        void connection();
+
+};
+
+#endif // CONNECTION_H
